7936c8a960852b14920cc87d8e85a5b4cc1177f5
riccardotota791-hash <riccardotota791@gmail.com>
2026-08-16 10:17:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,21 +3,17 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './Gemini_Generated_Image_ftfkw8ftfkw8ftfk.jfif'
+  './icon.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
